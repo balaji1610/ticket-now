ae771d43fb63425afffcf4f15e45ec76174ad932
Balaji <81224567+balaji1610@users.noreply.github.com>
2025-01-09 12:39:49 +0530

    implemented routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 import { useUserContext } from "./context/userContext";
+import { useRouter } from "next/navigation";
 export default function Home() {
   const { user } = useUserContext();
+  const router = useRouter();
+  const bookingTitle = "anirudh_show";
   return (
     <div>
       <h1>{user}</h1>
+      <button onClick={() => router.push(`/events`)}>Events</button>
+      <button onClick={() => router.push(`/events/${bookingTitle}`)}>
+        Go to booking
+      </button>
     </div>
   );
 }
