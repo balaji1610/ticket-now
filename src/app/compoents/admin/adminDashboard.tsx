6a9be8c441658d0afbc84cbd2e26afd9cd6cab8e
Balaji <81224567+balaji1610@users.noreply.github.com>
2025-01-10 11:11:45 +0530

    Add button

--- src/app/compoents/admin/adminDashboard.tsx
+++ src/app/compoents/admin/adminDashboard.tsx
@@ -1,19 +1,27 @@
 "use client";
+import Grid from "@mui/material/Grid2";
+import Sidebar from "./sidebar";
+import Box from "@mui/material/Box";
+
 type AdminDashboardProps = {
   children: React.ReactNode;
 };
 
-import Sidebar from "./sidebar";
-
 export default function AdminDashboard({ children }: AdminDashboardProps) {
   return (
-    <div>
-      <div>
-        <div>
+    <Grid container sx={{ marginTop: "8vh" }} spacing={2}>
+      <Grid size={{ xs: 1, sm: 1, md: 1, lg: 1 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
           <Sidebar />
-        </div>
-        <div> {children}</div>
-      </div>
-    </div>
+        </Box>
+      </Grid>
+      <Grid size={{ xs: 10, sm: 10, md: 10, lg: 10 }}>{children}</Grid>
+    </Grid>
   );
 }
--- src/app/compoents/admin/events.tsx
+++ src/app/compoents/admin/events.tsx
@@ -1,9 +1,31 @@
 "use client";
-
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+import Button from "@mui/material/Button";
 export default function Events() {
   return (
-    <div>
-      <div>EVENTSDETAILS</div>;
-    </div>
+    <Stack spacing={5}>
+      <Box>
+        <Grid
+          container
+          direction="row"
+          sx={{
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+          spacing={2}
+        >
+          <Grid size={{ xs: 5, md: 5 }}>
+            {" "}
+            <Button variant="contained" color="success">
+              Add Event
+            </Button>
+          </Grid>
+          <Grid size={{ xs: 5, md: 5 }}>Filter Events</Grid>
+        </Grid>
+      </Box>
+      <Box>Table</Box>
+    </Stack>
   );
 }
